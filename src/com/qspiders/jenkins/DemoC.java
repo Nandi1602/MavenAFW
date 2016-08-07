852120a8d7820b0fd60819e6acebfa035089740b
Nandi1602 <nandeesha.s@gmail.com>
2016-08-07 14:25:50 +0530

    Update DemoC.java
    changed the URL.

--- src/com/qspiders/jenkins/DemoC.java
+++ src/com/qspiders/jenkins/DemoC.java
@@ -9,7 +9,7 @@ public class DemoC {
 	public void test()
 	{
 		WebDriver driver = new FirefoxDriver();
-		driver.get("http://www.seleniumhq.org/download/");
+		driver.get("https://www.google.co.in/");
 		driver.close();
 	}
 
