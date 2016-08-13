855802410dc38e2bc668be59f01a871591615c55
Nandeesha <nandeesha.s@gmail.com>
2016-08-13 15:31:12 +0530

    added new class.

---
+++ src/com/qspiders/jenkins/DemoA.java
@@ -0,0 +1,16 @@
+package com.qspiders.jenkins;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA {
+	
+	@Test
+	public void test()
+	{
+		Reporter.log("Failed Test");
+		Assert.fail();
+	}
+
+}
