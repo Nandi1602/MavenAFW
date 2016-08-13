6ffaac58c2e5e04c55accff9e0be680b55c35e4b
Nandeesha <nandeesha.s@gmail.com>
2016-08-13 18:27:24 +0530

    added extent reports

--- src/com/qspiders/jenkins/DemoA.java
+++ src/com/qspiders/jenkins/DemoA.java
@@ -9,6 +9,7 @@ public class DemoA {
 	@Test
 	public void test()
 	{
+		
 		Reporter.log("Failed Test");
 		Assert.fail();
 	}
--- src/com/qspiders/jenkins/DemoC.java
+++ src/com/qspiders/jenkins/DemoC.java
@@ -5,14 +5,39 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class DemoC {
+	
+	ExtentReports report = new ExtentReports("./ExtentReport/ExtentReport.html",false);
+	
 	@Test
-	public void test()
+	public void extnreports()
 	{
-		WebDriver driver = new FirefoxDriver();
+		
+		ExtentTest logger = report.startTest("extnreports");
+		logger.log(LogStatus.PASS, "test passed");
+		report.endTest(logger);
+		//report.flush();
+		
+		/*WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.gmail.com/");
 		driver.close();
-		Reporter.log("Test Executed from Jenkins", true);
+		Reporter.log("Test Executed from Jenkins", true);*/
+	}
+	
+	@Test
+	public void test1()
+	{
+		
+		ExtentTest logger = report.startTest("test1");
+		logger.log(LogStatus.FAIL, "test failed");
+		report.endTest(logger);
+		report.flush();
+		
+		
 	}
 
 }
